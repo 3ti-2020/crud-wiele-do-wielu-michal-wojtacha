837821f641d4c6fdca27134658c708e5ca182d41
mwojtacha <42960630+mwojtacha@users.noreply.github.com>
2020-11-03 11:51:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -16,29 +16,6 @@
     <input type="text" name="tytul"> <br>
     <input type="submit" value="DODAJ"> <br>
     </form>
-    <?php
-
-    $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373162", "5YpM88aCen", "sql7373162");
-
-    $result_autor = $conn->query("SELECT * FROM lib_autor");
-    $result_tytuly = $conn->query("SELECT * FROM lib_tytul");
-
-    echo("<form action='krzyz.php' method='POST'  class='insert'>");
-    echo("<select name='autor'>");
-    while($wiersz=$result_autor->fetch_assoc() ){
-        echo("<option value='".$wiersz['id_autor']."'>".$wiersz['imie']."</option>");
-    }
-    echo("</select>");
-
-    echo("<select name='tytul'>");
-    while($wiersz=$result_tytuly->fetch_assoc() ){
-        echo("<option value='".$wiersz['id_tytul']."'>".$wiersz['tytul']."</option>");
-    }
-    echo("</select>");
-
-    echo("<input type='submit' value='DODAJ'>");
-    echo("</form>");
-    ?>
     </div>
     <div class="main">
     <?php
