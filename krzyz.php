883fa37e2f08e8daaa17a8db42fe8e7926c2b659
mwojtacha <42960630+mwojtacha@users.noreply.github.com>
2020-11-05 12:15:23 +0100

    Usun krzyz.php - niepotrzebny

--- krzyz.php
+++
@@ -1,6 +0,0 @@
-<?php
- $conn = new mysqli("remotemysql.com", "iLlI4Mu3Ym", "cjbNgHQp2x", "iLlI4Mu3Ym");
- $sql = "INSERT INTO lib_autor_tytul(id_autor_tytul, id_autor, id_tytul) VALUES (NULL , '".$_POST['autor']."', '".$_POST['tytul']."')";
- mysqli_query($conn, $sql);
- header('Location: https://crud-michal-wojtacha.herokuapp.com');
-?>
