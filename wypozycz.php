ad281cf7c5dfe7a1c8f33db37f635122293b9e03
mwojtacha <42960630+mwojtacha@users.noreply.github.com>
2020-11-12 14:20:50 +0100

    Wypozycz.php

--- wypozycz.php
+++ wypozycz.php
@@ -1,6 +1,7 @@
 <?php
  $conn = new mysqli("remotemysql.com", "iLlI4Mu3Ym", "cjbNgHQp2x", "iLlI4Mu3Ym");
- $id = $_POST['id'];
+ $id = $_POST['idtytul'];
+ $sql = "INSERT INTO `lib_wypoz` (`id_wypoz`, `id_tytul`, `id_uzytk`, `data_wypoz`, `data_odda`) VALUES (NULL,'$id','1',curdate(),curdate())";
  mysqli_query($conn, $sql);
  header('Location: https://crud-michal-wojtacha.herokuapp.com/logowanie.php');
 ?>
